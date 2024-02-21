25b3e4c1729da77b7a2f581b2d773f034853385a
Rafael Ribas <133046889+profRafaelRibas@users.noreply.github.com>
2024-02-21 10:37:11 -0300

    Desafio3

--- script.js
+++ script.js
@@ -6,3 +6,10 @@ function perguntar() {
     let nome = prompt("Qual é o seu nome?")
     alert(`Olá ${nome}`)
 }
+
+function mudar(){
+    let titulo = document.getElementsByTagName("h1")
+    alert(titulo[0].innerText)
+    console.log(titulo[0].innerText)
+    titulo[0].innerText = "Manipulei o DOM"
+}
