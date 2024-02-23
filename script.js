d2ef32f007a958fbf1cc0a7d86b10c787a49e1c8
Rafael Ribas <133046889+profRafaelRibas@users.noreply.github.com>
2024-02-23 00:40:54 -0300

    casa

--- script.js
+++ script.js
@@ -7,9 +7,54 @@ function perguntar() {
     alert(`Olá ${nome}`)
 }
 
-function mudar(){
+function mudar() {
     let titulo = document.getElementsByTagName("h1")
     alert(titulo[0].innerText)
     console.log(titulo[0].innerText)
     titulo[0].innerText = "Manipulei o DOM"
+}
+
+function validarFormulario() {
+    const nome = document.getElementById("nome").value
+    const email = document.getElementById("email").value
+    const idade = document.getElementById("idade").value
+    console.log(nome, email, idade)
+    if (nome && email && idade > 0) {
+        alert("Respostas enviadas com sucesso!")
+    } else {
+        alert("Complete as informações antes de enviar")
+    }
+}
+
+function mostrarConteudo(menuItem) {
+    const conteudo = document.getElementById("conteudo")
+    switch (menuItem) {
+        case 'HTML':
+            conteudo.innerHTML = "<p>Conteúdo sobre HTML.</p>"
+            break
+        case 'CSS':
+            conteudo.innerHTML = "<p>Conteúdo sobre CSS.</p>"
+            break
+        case 'JS':
+            conteudo.innerHTML = "<p>Conteúdo sobre JS.</p>"
+            break
+        default:
+            break
+    }
+}
+
+const numeroSecreto = Math.floor(Math.random() * 10) + 1;
+let tentativas = 0;
+
+function verificarAdivinhacao() {
+    const tentativaUsuario = parseInt(document.getElementById("tentativa").value);
+
+    if (tentativaUsuario === numeroSecreto) {
+        document.getElementById("feedback").innerText = `Parabéns! Você acertou em ${tentativas + 1} tentativas.`;
+    } else {
+        tentativas++;
+        document.getElementById("feedback").innerText = tentativaUsuario > numeroSecreto
+            ? "Tente um número menor."
+            : "Tente um número maior.";
+    }
 }
